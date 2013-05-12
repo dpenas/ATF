1ef211a447dc3ecc08598ffc03fb2912f5e64780
dario <dariosoyyo@gmail.com>
2013-05-13 00:20:23 +0200

    Made some changes to the algorithm

--- bergerchecksum/checksumhoney.py
+++ bergerchecksum/checksumhoney.py
@@ -1,33 +1,21 @@
-def sum_wordsleft (a, c):
-	result = list("0000")
+from random import randint
+def randomoneorzero(length):
+	aux = ""
+	for i in range(length):
+		aux = aux + str(randint(0,1))
+	return aux
+
+def sum_words (a, b, length):
+
+	result = list(randomoneorzero(length))
 	check = 0
 	for i in range(len(a)-1, -1, -1):
-		if ((a[i] == "0") and (c[i] == "0")):
+		if ((a[i] == "0") and (b[i] == "0")):
 			result[i] = "0"
 			if (check):
 				result[i] = "1"
 				check = 0
-		elif (((a[i] == "1") and (c[i] == "0")) or ((a[i] == "0") and (c[i] == "1"))):
-			result[i] = "1"
-			if (check):
-				result[i] = "0"
-				check = 1
-		else:
-			result[i] = "0"
-			if (check):
-				result[i] = "1"
-			check = 1
-	return "".join(result)
-def sum_wordsright (b, d):
-	result = list("0000")
-	check = 0
-	for i in range(len(b)-1, -1, -1):
-		if ((b[i] == "0") and (d[i] == "0")):
-			result[i] = "0"
-			if (check):
-				result[i] = "1"
-				check = 0
-		elif (((b[i] == "1") and (d[i] == "0")) or ((b[i] == "0") and (d[i] == "1"))):
+		elif (((a[i] == "1") and (b[i] == "0")) or ((a[i] == "0") and (b[i] == "1"))):
 			result[i] = "1"
 			if (check):
 				result[i] = "0"
@@ -40,7 +28,10 @@ def sum_wordsright (b, d):
 	return "".join(result)
 
 def codify(a, b, c, d):
-	right = sum_wordsright(a,c)
-	left = sum_wordsleft(b,d)
+	basenumber = randomoneorzero(len(a)*2) 
+	right = sum_words(a, c, len(c))
+	left = sum_words(b, d, len(a))
 	final = left + right
-	return final
+	print final
+	realfinal = sum_words(basenumber, final, len(basenumber))
+	print realfinal
